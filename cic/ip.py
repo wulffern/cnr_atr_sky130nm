a669004a2aeda14b5f58e39a8decc65c45db96af
Carsten Wulff <carsten@wulff.no>
2023-05-29 17:22:17 +0200

    Added python code to generate transistors

--- cic/ip.py
+++ cic/ip.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 
+name = "{type}CH_{contacts}C{f_int}F{f_frac}"
 
 cell = """
-        { "name" : "{type}CH_{contacts}C{f_int}F{f_frac}",
+        { "name" : "{name}",
           "inherit" : "{type}CH",
           "abstract" : 0,
           "verticalMultiplyVector" : [1.2,1,{f},1,{f},1,1.2],
@@ -24,20 +25,27 @@ lengths = [1.2,2,4,8,12]
 
 #- Generate all transistors
 cells = list()
+names = list()
 for t in types:
     for c in contacts:
         for f in lengths:
             f_frac = (f % 1)*10
-            print(f_frac)
-            ss = cell.replace("{type}",t) \
+            cname = name.replace("{type}",t) \
             .replace("{contacts}",str(c)) \
-            .replace("{count}",str(int(c/2-1))) \
-            .replace("{f}",str(f)) \
             .replace("{f_int}",str(int(f))) \
             .replace("{f_frac}",str(round(f_frac)))
+            names.append(cname)
+
+            ss = cell.replace("{type}",t) \
+            .replace("{name}",cname) \
+            .replace("{count}",str(int(c/2-1))) \
+            .replace("{f}",str(f))
             cells.append(ss)
 
 N = len(cells)
+
+with open("../cic/cells","w") as fo:
+    fo.write("CELLS = " + " ".join(names))
 
 with open("../cic/alltran.json","w") as fo:
     fo.write("""
